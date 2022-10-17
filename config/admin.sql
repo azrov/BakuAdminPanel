40fede393e3807d0ad38e5f4040e4cb2544a34a1
Ali <alizairov@mail.ru>
2022-10-17 21:48:33 +0400

    config

---
+++ config/admin.sql
@@ -0,0 +1,209 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3307
+-- Generation Time: Oct 16, 2022 at 12:59 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.0.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `admin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modules`
+--
+
+CREATE TABLE `modules` (
+  `id` int(3) NOT NULL,
+  `role` int(1) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `author` varchar(25) NOT NULL,
+  `path` varchar(50) NOT NULL,
+  `icon` varchar(25) NOT NULL,
+  `version` varchar(10) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `blocked` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `modules`
+--
+
+INSERT INTO `modules` (`id`, `role`, `name`, `description`, `author`, `path`, `icon`, `version`, `status`, `blocked`) VALUES
+(1, 1, 'dashboard', 'Dashboard', 'Ali Zairov', 'Modules/Admin/Dashboard', 'activity', '1.0', 1, 1),
+(2, 1, 'users', 'Users', 'Ali Zairov', 'Modules/Admin/Dashboard/Users', 'users', '1.0', 1, 1),
+(3, 1, 'roles', 'Roles', 'Ali Zairov', 'Modules/Admin/Dashboard/Roles', 'user', '1.0', 1, 1),
+(4, 1, 'settings', 'Settings', 'Ali Zairov', 'Modules/Admin/Dashboard/Settings', 'settings', '1.0', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `id` int(3) NOT NULL,
+  `role` int(1) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(3) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`) VALUES
+(1, 'admin'),
+(2, 'manager'),
+(3, 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `id` int(1) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `keywords` varchar(150) NOT NULL,
+  `author` varchar(50) NOT NULL,
+  `icon` varchar(25) NOT NULL,
+  `version` varchar(15) NOT NULL,
+  `underrepair` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `title`, `description`, `keywords`, `author`, `icon`, `version`, `underrepair`) VALUES
+(1, 'Baku Admin Panel', 'Baku Admin Panel', 'Baku,Admin,Panel', 'Ali Zairov', 'favicon.ico', '1.0', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(9) NOT NULL,
+  `role` int(1) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `avatar` varchar(50) NOT NULL,
+  `banned` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `role`, `name`, `surname`, `username`, `email`, `password`, `avatar`, `banned`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Ali', 'Zairov', 'azrov', 'alizairov@mail.ru', 'd6afc6e58e7845740a21da2b4e07381d', 'azrov.jpg', 0, '2022-09-25 18:47:09', '2022-10-16 10:17:58');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `modules`
+--
+ALTER TABLE `modules`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `modules`
+--
+ALTER TABLE `modules`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
